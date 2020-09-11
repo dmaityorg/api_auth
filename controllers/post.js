6c2f5951b080a5510ea09998425678675f0a37d5
Debasish Maity <dmaity@celestialsys.com>
2020-09-11 17:47:12 +0530

    Remove unnesseary code

--- controllers/post.js
+++ controllers/post.js
@@ -45,31 +45,4 @@ module.exports = {
       .then((post) => res.status(201).send(post))
       .catch((error) => res.status(400).send(error));
   },
-
-  // update(req, res) {
-  //   return Profile
-  //     .findById(req.params.id, {
-  //       include: [{
-  //         model: User,
-  //         as: 'User'
-  //       }],
-  //     })
-  //     .then(profile => {
-  //       if (!profile) {
-  //         return res.status(404).send({
-  //           message: 'Profile Not Found',
-  //         });
-  //       }
-  //       return profile
-  //         .update({
-  //           user_id: req.body.user_id || classroom.user_id,
-  //           fullname: req.body.fullname || classroom.fullname,
-  //           birthdate: req.body.birthdate || classroom.birthdate,
-  //           gender: req.body.gender || classroom.gender
-  //         })
-  //         .then(() => res.status(200).send(profile))
-  //         .catch((error) => res.status(400).send(error));
-  //     })
-  //     .catch((error) => res.status(400).send(error));
-  // },
 };
